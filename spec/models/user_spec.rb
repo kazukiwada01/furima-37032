595ee572ec15b6e05c53095ef31f589ac5605602
kazukiwada01 <92724990+kazukiwada01@users.noreply.github.com>
2021-12-16 19:48:02 +0900

    ユーザー管理機能実装の修正(1)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,6 +54,11 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
       end
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'パスワード1234'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
+      end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password_confirmation = ''
         @user.valid?
