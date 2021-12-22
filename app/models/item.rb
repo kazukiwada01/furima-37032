631360204b04d819753b752feb10ea25727460fd
kazukiwada01 <92724990+kazukiwada01@users.noreply.github.com>
2021-12-22 19:54:35 +0900

    商品出品機能実装の修正(1)

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,14 +9,18 @@ class Item < ApplicationRecord
   belongs_to_active_hash :ship_from
   belongs_to_active_hash :ship_day
 
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :ship_from_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :ship_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+  end
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :ship_from_id
+    validates :ship_day_id
+  end
   validates :price, presence: true
   validates :price, numericality: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }
   validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
