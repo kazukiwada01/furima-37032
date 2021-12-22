bc021ad4a3cd82efec1062698deeda4667332fc0
kazukiwada01 <92724990+kazukiwada01@users.noreply.github.com>
2021-12-22 20:18:09 +0900

    商品出品機能実装の修正(2)

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,5 +23,5 @@ class Item < ApplicationRecord
   end
   validates :price, presence: true
   validates :price, numericality: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }
-  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 end
